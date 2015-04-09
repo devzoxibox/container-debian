f6d0f4c80db87cbb57d19ff21c17182de2400f1b
zoxibox <djzoxi@gmail.com>
2015-04-09 11:00:22 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Mises à jour
+apt-get update && apt-get -qy dist-upgrade 
+apt-get -q update && apt-get -qy install wget locales
+apt-get clean
+
+# Locales
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && locale-gen
+
+# User nobody pour UnRAID
+usermod -u 99 nobody
+usermod -g 100 nobody
+
+
